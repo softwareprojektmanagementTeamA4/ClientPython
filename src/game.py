933155f8c427787ec11b0220128f6cb6bcac6d3d
David Stevic <stevic.d@outlook.de>
2023-12-03 14:13:10 +0100

    Bugfix für Online-User-Anzeige und Kommentare hinzugefügt

--- src/game.py
+++ src/game.py
@@ -16,9 +16,21 @@ connectmenue = True
 menueactive = False
 connectEnter = False
 
+username_input_box = menue.TextInputBox(WIDTH/2, (HEIGHT/2) - 70, 400, font)
+input_group = pygame.sprite.Group(username_input_box)
+
 sio = socketio.Client()
 username = None
 usernames = None
+
+"""
+SockeIO Eventhandler
+
+@sio.connect: Informiert über erfolgreiche Verbindung
+@sio.connect_error: Informiert über fehlerhafte Verbindung
+@sio.disconnect: Informiert über Trennung der Verbindung
+@sio.playersConnected: Informiert über die Verbindung anderer Spieler und speichert diese ins Dictionary 'usernames'
+"""
 @sio.event
 def connect():
     print("I'm connected! ")
@@ -42,9 +54,7 @@ def playersConnected(data):
     usernames = data['usernames']
     print('Updated Users:', usernames)
 
-username_input_box = menue.TextInputBox(WIDTH/2, (HEIGHT/2) - 70, 400, font)
-input_group = pygame.sprite.Group(username_input_box)
-
+# Gameloop für das Ausführen des Programms
 run = True
 while run:
     screen.fill('blue')
@@ -55,13 +65,14 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
+    # Der User muss sich mit dem Server Connecten und ist somit im Login Screen.
     if connectmenue:
         input_group.draw(screen)
 
         connect_button = menue.Button('Connect', WIDTH/2, HEIGHT/2, screen)
         connect_button.draw()
-
-        if connect_button.button.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0] == True or username_input_box.enterpressed:
+        # Prüft ob der User den Connect-Button gedrückt hat oder das Eingabefeld durch Enter bestätigt hat
+        if connect_button.button.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0] or username_input_box.enterpressed:
             connectmenue = False
             username = username_input_box.text
             try:
@@ -69,7 +80,8 @@ while run:
                 pygame.time.delay(1000)
             except Exception as ex:
                 print("Verbindungsfehler")
-
+            # Wenn die Verbindung erfolgreich war, geht es ins Hauptmenü.
+            # Andernfalls wird der User über die fehlerhafte Verbindung informiert und geht zurück in den Login Screen.
             if sio.connected:
                 menueactive = True
             else:
@@ -80,14 +92,16 @@ while run:
                 pygame.time.delay(3000)
                 connectmenue = True
                 username_input_box.enterpressed = False
+    # Der User ist im Hauptmenü.
     if menueactive:
+        # Zeichnet alle User die Online sind, oben links in die Ecke
         status = font.render('Connected to Server ', True, 'black')
         screen.blit(status, (0,0))
         i = 25
         for users in usernames:
             online = font.render(users + ' Online', True, 'green')
             screen.blit(online, (0, i))
-            i = i*2
+            i += 25
 
         start_button = menue.Button('Start', WIDTH/2, (HEIGHT/2) - 65, screen)
         start_button.draw()
@@ -96,13 +110,14 @@ while run:
         quit_button = menue.Button('Quit', WIDTH / 2, (HEIGHT/2) + 65, screen)
         quit_button.draw()
 
-        if start_button.button.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0] == True:
+        # Prüft die Betätigung der Hauptmenü-Buttons.
+        if start_button.button.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0]:
             ## Starte das Spiel
             pass
-        if settings_button.button.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0] == True:
+        if settings_button.button.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0]:
             ## Zeige die Einstellungen
             pass
-        if quit_button.button.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0] == True:
+        if quit_button.button.collidepoint(pygame.mouse.get_pos()) and pygame.mouse.get_pressed()[0]:
             run = False
 
     pygame.display.flip()
--- src/menue.py
+++ src/menue.py
@@ -1,5 +1,15 @@
 import pygame
 
+"""
+Class Button
+
+Erstellt bei Initialisierung  ein Rechteck, was als Button dient.
+@:param
+txt: Text der auf dem Button stehen soll
+x: x Koordinate
+y: y Koordinate
+screen: Container/Leinwand auf dem der Button gezeichnet werden soll
+"""
 class Button():
     def __init__(self, txt, x, y, screen):
         self.font = pygame.font.SysFont('Georgia', 24, bold=False)
@@ -10,12 +20,27 @@ class Button():
         self.button = pygame.rect.Rect(x,y,150,60)
         self.button.midbottom = (x,y)
 
+    """
+    def draw
+    
+    Bei Aufruf wird der Button gezeichnet.
+    """
     def draw(self):
         btn = pygame.draw.rect(self.screen, 'light gray', self.button, 0, 5)
         btnrand = pygame.draw.rect(self.screen, 'black', self.button, 5, 5)
         surf = self.font.render(self.text, True, 'black')
         self.screen.blit(surf, (self.button.midleft[0] + 25, self.button.midleft[1] - 15))
 
+"""
+Class TextInputBox
+
+Eine Sprite-Klasse, die ein Eingabefeld für den User erstellt.
+@:param
+x: x Koordinate
+y: y Koordinate
+w: Breite des Eingabefeldes
+font: Die Schriftart, in der die Eingabe dargestellt wird
+"""
 class TextInputBox(pygame.sprite.Sprite):
     def __init__(self, x, y, w, font):
         super().__init__()
@@ -29,6 +54,11 @@ class TextInputBox(pygame.sprite.Sprite):
         self.render_text()
         self.enterpressed = False
 
+    """
+    def render_text
+
+    Zeichnet den aktuellen Stand der Eingabe.
+    """
     def render_text(self):
         t_surf = self.font.render(self.text, True, self.color, self.backcolor)
         self.image = pygame.Surface((max(self.width, t_surf.get_width()+10), t_surf.get_height()+10), pygame.SRCALPHA)
@@ -38,6 +68,13 @@ class TextInputBox(pygame.sprite.Sprite):
         pygame.draw.rect(self.image, self.color, self.image.get_rect().inflate(-2, -2), 2)
         self.rect = self.image.get_rect(midbottom = self.pos)
 
+    """
+    def update
+
+    Wird in der Gameloop 60-mal die Sekunde aufgerufen, um die Usereingabe zu erfassen und ruft render_text zum Updaten auf.
+    @:param
+    event_list: Die Eventliste von Pygame, die die benötigten Tastatureingaben speichert
+    """
     def update(self, event_list):
         for event in event_list:
             if event.type == pygame.MOUSEBUTTONDOWN and not self.active:
